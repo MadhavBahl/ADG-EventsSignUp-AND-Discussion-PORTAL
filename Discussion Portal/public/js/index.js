7f7e39cc930d8cd403b38b06d575eb2a92ffcdb5
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-08 23:22:01 +0530

    Working on data updation

--- Discussion Portal/public/js/index.js
+++ Discussion Portal/public/js/index.js
@@ -16,42 +16,67 @@ var ref = database.ref('Forum');
 ref.on('value',getData,errData);
 var question = document.getElementById('question');
 var nameTEST = document.getElementById('nameTEST');
+var flag = 0;
+var approveBtn = document.getElementById('approveBtn');
+var disapproveBtn = document.getElementById('disapproveBtn');
 
 var dispErr = document.getElementById('dispErr');
 function getData(data){
   console.log(data.val());
 
-  // var testMainPortal = document.createElement('div');
-  // testMainPortal.id = mainPortal;
-  // document.getElementsByTagName('body')[0].appendChild(testMainPortal);
-  // var testQuestionPortal = document.createElement('div');
-  // testQuestionPortal = questionPortal;
-  // testMainPortal.appendChild(testQuestionPortal);
-  // var testQuestion = document.createElement('h1');
-  // testQuestion.id = question;
-  // testQuestionPortal.appendChild(testQuestion);
-  // testQuestion.innerHTML = "Fetch your question Here";
+  /* ======== Try For Array Structure ======== */
 
 
-
-  /* ======== Try For Array SAtructure ======== */
-
-
-    var users = [];
+    // var users = [];
     for(var i in data.val()) {
       // setTimeout(function(){
+      //   console.log(`The key is: ${i}`);
+      //   var dat =i.toString();
+      //   var testCase = data.val().dat;
+      //   // question.innerHTML = testCase.question;
+      //   // nameTEST.innerHTML = testCase.name;
+      //   console.log(testCase);
+      // },1000);
+
+      flag = 0;
+      if(data.val()[i].verified === 'Yes')
+        flag=1;
+      else{
+        question.innerHTML = data.val()[i].question;
+        nameTEST.innerHTML = data.val()[i].name;
+        var flagFin=0;
+        while(flagFin == 0){
+          if(approveBtn.onclick){
+            approved(i);
+            flagFin = 1;
+          } else if(disapproveBtn.onclick){
+            disapproved(i);
+            flagFin = 1;
+          }
+        }
+      }
+      // if(flag==0){
+      //   console.log(`The key is: ${i}`);
+      //   console.log(`The name is: ${data.val()[i].name}`);
+      //   question.innerHTML = testCase.question;
+      //   nameTEST.innerHTML = testCase.name;
       //
-      // },2000);
-      users.push(i);
+      // }
+      // users.push(i);
     }
-    console.log(users);
+    // console.log(users);
+    // for(var i=0;i<users.length;i++){
+    //   // console.log(users[i]);
+    //   var fetch = users[i];
+    //   console.log(data.val()[fetch]);
+    // }
 
 
   /* ========================================== */
 
-  var testCase = data.val().TEST;
-  question.innerHTML = testCase.question;
-  nameTEST.innerHTML = testCase.name;
+  // var testCase = data.val().TEST;
+  // question.innerHTML = testCase.question;
+  // nameTEST.innerHTML = testCase.name;
 }
 
 function errData(err){
@@ -60,9 +85,11 @@ function errData(err){
   dispErr.innerHTML = errMsg;
 }
 
-function approved(){
-  database.ref('Forum/TEST/verified').set('Yes');
+function approved(path){
+  database.ref('Forum/'+path+'/verified').set('Yes');
+  flag =1;
 }
-function disapproved(){
-  database.ref('Forum/TEST/verified').set('No');
+function disapproved(path){
+  database.ref('Forum/'+path+'/verified').set('No');
+  flag =1;
 }
