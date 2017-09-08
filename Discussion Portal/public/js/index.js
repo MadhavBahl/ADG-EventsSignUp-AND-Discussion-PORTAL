f875059ec56dc0612150b29da24eb145073c51d7
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-08 22:39:26 +0530

    Trying dynamic DOM manipulation

--- Discussion Portal/public/js/index.js
+++ Discussion Portal/public/js/index.js
@@ -19,7 +19,36 @@ var nameTEST = document.getElementById('nameTEST');
 
 var dispErr = document.getElementById('dispErr');
 function getData(data){
-  console.log(data.val().TEST);
+  console.log(data.val());
+
+  // var testMainPortal = document.createElement('div');
+  // testMainPortal.id = mainPortal;
+  // document.getElementsByTagName('body')[0].appendChild(testMainPortal);
+  // var testQuestionPortal = document.createElement('div');
+  // testQuestionPortal = questionPortal;
+  // testMainPortal.appendChild(testQuestionPortal);
+  // var testQuestion = document.createElement('h1');
+  // testQuestion.id = question;
+  // testQuestionPortal.appendChild(testQuestion);
+  // testQuestion.innerHTML = "Fetch your question Here";
+
+
+
+  /* ======== Try For Array SAtructure ======== */
+
+
+    var users = [];
+    for(var i in data.val()) {
+      // setTimeout(function(){
+      //
+      // },2000);
+      users.push(i);
+    }
+    console.log(users);
+
+
+  /* ========================================== */
+
   var testCase = data.val().TEST;
   question.innerHTML = testCase.question;
   nameTEST.innerHTML = testCase.name;
