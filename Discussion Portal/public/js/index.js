c4cbb2d6d8c8dd0672040635d35b667eba4c84a2
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-08 21:42:26 +0530

    Fetching Forum Data

---
+++ Discussion Portal/public/js/index.js
@@ -0,0 +1,32 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCGWZaO0w87ASD2B7yZhtAoOGb8mhlwUlc",
+  authDomain: "iosfusion-21c51.firebaseapp.com",
+  databaseURL: "https://iosfusion-21c51.firebaseio.com",
+  projectId: "iosfusion-21c51",
+  storageBucket: "iosfusion-21c51.appspot.com",
+  messagingSenderId: "342302240126"
+};
+firebase.initializeApp(config);
+// console.log(firebase);
+
+var database = firebase.database();
+var ref = database.ref('Forum');
+ref.on('value',getData,errData);
+var question = document.getElementById('question');
+var nameTEST = document.getElementById('nameTEST');
+
+var dispErr = document.getElementById('dispErr');
+function getData(data){
+  console.log(data.val().TEST);
+  var testCase = data.val().TEST;
+  question.innerHTML = testCase.question;
+  nameTEST.innerHTML = testCase.name;
+}
+
+function errData(err){
+  console.log(err);
+  var errMsg = `Unable to fetch data!! ERROR: ${err}`;
+  dispErr.innerHTML = errMsg;
+}
