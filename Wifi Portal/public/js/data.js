832da61db96b130892f9d142e1b9cebcaccc449c
MADHAV BAHL <madhavbahl10@gmail.com>
2017-09-09 21:49:29 +0530

    Wifi Portal Done

--- Wifi Portal/public/js/data.js
+++ Wifi Portal/public/js/data.js
@@ -15,20 +15,44 @@
 
 
 
+  // $('.tab a').on('click', function (e) {
+  //
+  //   e.preventDefault();
+  //
+  //   $(this).parent().addClass('active');
+  //   $(this).parent().siblings().removeClass('active');
+  //
+  //   target = $(this).attr('href');
+  //
+  //   $('.tab-content > div').not(target).hide();
+  //
+  //   $(target).fadeIn(600);
+  //
+  // });
+
+
+
   function getData(data){
-    console.log(data.val());
+    //console.log(data.val());
     document.getElementById("sbmt").addEventListener("click",evt => {
       evt.preventDefault();
       var regNo = document.getElementById('regNo').value;
       console.log(data.val()[regNo]);
       // console.log(evt);
       // var user = 'User';
+      if(!data.val()[regNo]){
+        alert("Please Enter The Correct Registration Number!!!");
+      }
       console.log(data.val()[regNo].PIN);
       console.log(data.val()[regNo].User);
       var formStr = document.getElementById('formMain');
       var dispData = document.getElementById('dispData');
       dispData.style.display = "block";
       formStr.style.display = "none";
+      var userName = document.getElementById('userName');
+      var pin = document.getElementById('pin');
+      userName.innerHTML = data.val()[regNo].User;
+      pin.innerHTML = data.val()[regNo].PIN;
     });
 
     // console.log()
